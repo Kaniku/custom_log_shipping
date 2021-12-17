dc3dda9d8c7398038a0b69f6f146e5f2dd4c9a23
kaniku <katarinanimas20@gmail.com>
2021-12-17 21:20:01 +0700

    backup script

---
+++ backup.py
@@ -0,0 +1,47 @@
+import pyodbc
+import datetime
+import pymongo
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+connSqlServer = pyodbc.connect(driver='{SQL Server Native Client 11.0}',
+                               server='127.0.0.1,1433',
+                               database='master',
+                               uid='sa',pwd='AEdCC.b9',
+                               autocommit=True)
+
+log_time = datetime.datetime.now().strftime("%d%m%y_%H%M%S")
+
+log_name = f'programming-in-db_log_{log_time}'
+
+backup = f"BACKUP LOG [programming-in-db] TO DISK= N'C:\kaniku_backup\{log_name}.bak'"
+cursor = connSqlServer.cursor().execute(backup)
+
+connSqlServer.close()  # close the connection
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["prod"]
+
+mycol = mydb["log_name_mdb"]
+
+mydict = {"log_name": log_name}
+
+x = mycol.insert_one(mydict)
+
+print(x.inserted_id)
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+upload_file_list = [f'/kaniku_backup/{log_name}.bak']
+for upload_file in upload_file_list:
+    gfile = drive.CreateFile({'parents': 
+    [{'id': '1dB8qNwSgEtaivSl3aKc3de4ZBiynaCpn'}]})
+    gfile.SetContentFile(upload_file)
+    gfile.Upload() #Upload file
+
+file_list = drive.ListFile({'q': "'{}' in parents and trashed=false".format('1dB8qNwSgEtaivSl3aKc3de4ZBiynaCpn')}).GetList()
+for file in file_list:
+    print('title: %s, id: %s' %(file['title'], file['id']))
